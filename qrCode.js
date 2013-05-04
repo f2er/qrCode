3792257cdc6d5f24c08e9d6e7ad8d6b6456904a3
f2er <uezheng@gmail.com>
2013-05-04 20:01:22 +0800

    update js

--- qrCode.js
+++ qrCode.js
@@ -1,8 +1,19 @@
 define(function(require,exports,module){
+	function _$(id){
+		return typeof id == "string" ? document.getElementById(id) : id;
+	}
 	function qrCode(urlId){
-		var _url = document.getElementById(urlId).value;
-		document.getElementById('j-qr-url').innerHTML = "https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=L|4&chl="+_url;
-		document.getElementById('j-qr-img').src="https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=L|4&chl="+_url;
+		var _url = _$(urlId).value;
+		if(_url==""){
+			_$('j-tip').innerHTML = _$('j-tip').getAttribute('data-tip');
+			return;
+		}
+		if(_$('j-tip').innerHTML != ""){
+			_$('j-tip').innerHTML = "";
+		}
+		_$('j-qr-url').innerHTML = "https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=L|4&chl="+_url;
+		_$('j-qr-img').src="https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=L|4&chl="+_url;
+		
 	};
 	module.exports = qrCode;
 });
