aa6ce0800f4eb4a64d158493e61bd1bef6f5c1a1
f2er <uezheng@gmail.com>
2013-05-04 21:05:09 +0800

    add chrome extension

---
+++ qr/qrcode.js
@@ -0,0 +1,21 @@
+(function(){
+	function _$(id){
+		return typeof id == "string" ? document.getElementById(id) : id;
+	}
+	function qrCode(urlId){
+		var _url = _$(urlId).value;
+		if(_url==""){
+			_$('j-tip').innerHTML = _$('j-tip').getAttribute('data-tip');
+			return;
+		}
+		if(_$('j-tip').innerHTML != ""){
+			_$('j-tip').innerHTML = "";
+		}
+		_$('j-qr-url').innerHTML = "https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=L|4&chl="+_url;
+		_$('j-qr-img').src="https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=L|4&chl="+_url;
+		
+	};
+	document.getElementById('j-btn-qr').onclick = function(){
+		qrCode('j-url');
+	};
+})();
